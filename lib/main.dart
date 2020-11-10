316ee3dd01c71ede3060caff9266f0db53a5cc00
sahil1sk <khannasahil303@gmail.com>
2020-11-10 15:39:01 +0530

    Building a widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ class MyApp extends StatelessWidget {
   // setting that this is the override method
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'));
+    //Scaffold give the basic design structure which more look like regular ui
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('My First App'),),
+        body: Text('This is my default text!'),
+      ),
+    );
   }
 }

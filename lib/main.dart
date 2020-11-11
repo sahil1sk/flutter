71566572adb370136cace1a0414d0a486086dfaf
sahil1sk <khannasahil303@gmail.com>
2020-11-11 06:30:25 +0530

    Connecting funtions with buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 
-// we can also run the funtion using => 
-// if we have to execute only one line of code
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen');
+  }
 
-  // setting that this is the override method
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My First App'),),
         body: Column(
           children: <Widget>[
             Text('What\'s your favorite color?'),
-            RaisedButton(child: Text('Answer 1'), onPressed: null, ),
-            RaisedButton(child: Text('Answer 2'), onPressed: null, ),
-            RaisedButton(child: Text('Answer 3'), onPressed: null, ),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion, ),
+            RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion, ),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion, ),
           ],
         ),
       ),

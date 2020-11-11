e9cf1a999a938ffc185c250cd30a535b6650f076
sahil1sk <khannasahil303@gmail.com>
2020-11-11 06:58:11 +0530

    Updating Widget data or using stateless widget incorrectly

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,26 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  var questionIndex = 0;
+
   void answerQuestion()  {
+    questionIndex++;
     print('Answer chosen');
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\s your favorite color?',
+      'What\s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('My First App'),),
         body: Column(
           children: <Widget>[
-            Text('What\'s your favorite color?'),
+            Text(questions.elementAt(questionIndex)),
             RaisedButton(child: Text('Answer 1'), onPressed: () => answerQuestion, ),
             RaisedButton(child: Text('Answer 2'), onPressed: () => print('Answered'), ),
             RaisedButton(child: Text('Answer 3'), onPressed: () {
